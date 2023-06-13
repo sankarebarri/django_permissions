e162a15e48926cf0e63d72be2b681cd6517c03ac
sankarebarri <sankarebarri_dev@yahoo.com>
2023-06-13 18:44:39 +0000

    has_permission functions added

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http.request import HttpRequest
 from . models import Product
 
 # admin.site.register(Product)
@@ -7,10 +8,23 @@ from . models import Product
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", )
 
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        is_superuser = request.user.is_superuser
+    # def get_form(self, request, obj=None, **kwargs):
+    #     form = super().get_form(request, obj, **kwargs)
+    #     is_superuser = request.user.is_superuser
 
-        if not is_superuser:
-            form.base_fields['name'].disabled = True
-        return form
+    #     if not is_superuser:
+    #         form.base_fields['name'].disabled = True
+    #     return form
+
+
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+    
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+    def has_view_permission(self, request, obj=None):
+        return True
