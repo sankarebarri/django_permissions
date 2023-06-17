6befcbccc712465217eafc3fb49f63b46f45639d
sankarebarri <sankarebarri_dev@yahoo.com>
2023-06-17 18:50:59 +0000

    django_guardian

--- inventory/admin.py
+++ inventory/admin.py
@@ -78,6 +78,7 @@
 from django.contrib import admin
 from .models import Product
 from guardian.admin import GuardedModelAdmin
+from guardian.shortcuts import get_objects_for_user
 
 # admin.site.register(Product)
 @admin.register(Product)
@@ -88,16 +89,42 @@ class ProductAdmin(GuardedModelAdmin):
         # return super().has_module_permission(request)
         if super().has_module_permission(request):
             return True
+        return self.get_model_objects(request).exists()
         
     def get_queryset(self, request):
-        return super().get_queryset(request)
+        if request.user.is_superuser:
+            return super().get_queryset(request)
+        data = self.get_model_objects(request)
+        return data
     
+    def has_permission(self, request, obj, action):
+        opts = self.opts
+        code_name = f'{action}_{opts.model_name}'
+        if obj:
+            return request.user.has_perm(f'{opts.app_label}.{code_name}', obj)
+        else:
+            return True
+    
+    def get_model_objects(self, request, action=None, klass=None):
+        opts = self.opts
+        actions = [action] if action else ['view', 'edit', 'delete']
+        klass = klass if klass else opts.model
+        model_name = klass._meta.model_name
+        return get_objects_for_user(user=request.user, perms=[f'{perm}_{model_name}' for perm in actions], klass=klass, any_perm=True)
+
+    ### This works on the second layer -- The list of model objects
+    # def has_view_permission(self, request, obj=None):
+    #     return True
     def has_view_permission(self, request, obj=None):
-        return True
+        return self.has_permission(request, obj, 'view')
     
+    # def has_change_permission(self, request, obj=None):
+    #     return True
     def has_change_permission(self, request, obj=None):
-        return True
+        return self.has_permission(request, obj, 'change')
     
+    # def has_delete_permission(self, request, obj=None):
+    #     return True
     def has_delete_permission(self, request, obj=None):
-        return True
+        return self.has_permission(request, obj, 'delete')
 
