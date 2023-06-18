165c0094e355ebc1f58fbbda07d74d557dd86187
sankarebarri <sankarebarri_dev@yahoo.com>
2023-06-18 15:27:59 +0000

    MLP within views and templates | project app created

--- django_permissions/settings.py
+++ django_permissions/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
+    'project',
     'guardian',
 ]
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'django_permissions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +127,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend', # this is default
